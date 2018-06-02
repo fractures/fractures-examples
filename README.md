e2db77dddbab1aee5a29e175e3ebcc6a0295671a
Krisztian Puska <pyx@pyx.space>
2018-06-03 00:13:34 +0200

    Update example structure, update cra

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fractures examples
+
+How to use [fractures](https://github.com/fractures/fractures) with your app. Request a new one [here](https://github.com/fractures/fractures-examples/issues).
+
+## Examples
+
+    - [fractures-react](./fractures-react)
+
+## Soon
+
+    - [fractures-next](./fractures-next)
+    - [fractures-ui-next](./fractures-ui-next)
+    - [fractures-ui-react](./fractures-ui-react)
---
+++ fractures-react/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+class App extends Component {
+	render() {
+		const emojis = [`🐀`, `🐛`, `🍅`, `🐞`, `🎱`, `🍃`]
+
+		return (
+			<div className="my-container flex flex-ycenter minh-viewport">
+				<div className="grid grid-cols-3 grid-gap-2 w-100">
+					{emojis.map((emoji, key) => (
+						<div className="p-3 flex flex-center bg-smoke" key={key}>
+							{emoji}
+						</div>
+					))}
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App
